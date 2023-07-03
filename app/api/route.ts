4e8eceb3650c58526b4299d894d8c7a8bc5e6815
tagoso <shirouo4279@gmail.com>
2023-07-04 01:38:51 +0900

    added route.ts

---
+++ app/api/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+ 
+export async function GET()  {
+    const res = await fetch(`https://api.fastforex.io/fetch-all?api_key=${process.env.NEXT_PUBLIC_FASTFOREX_API_KEY}`);
+  
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const ForexData = await res.json();
+    return NextResponse.json({ ForexData })
+  }
+  
