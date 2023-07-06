b5e24b268f236606be3a8fab4b26b707a0f502de
tagoso <shirouo4279@gmail.com>
2023-07-07 03:06:07 +0900

    added midleware

---
+++ app/api/middleware.ts
@@ -0,0 +1,20 @@
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+// Check if the request is from an allowed origin
+const allowedOrigins = ['http://localhost:3000', 'https://addcurr.tago.so/'];
+const { origin } = new URL(request.url);
+
+if (!allowedOrigins.includes(origin)) {
+    // If the origin is not allowed, respond with a 403 status
+    return new NextResponse(null, { status: 403 });
+}
+  
+  // If the origin is allowed, continue with the request
+  return NextResponse.next();
+}
+export const config = {
+    matcher: '/api/*',
+}
