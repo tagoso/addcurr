8d85d31341edc6b922f5cf069d869702c990af87
tagoso <shirouo4279@gmail.com>
2024-06-30 13:53:40 +0300

    Added space in the intro paragraph.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <>
       <header className="m-0 items-center">
-        AddCurr - the simplest portfolio maker.
+        AddCurr - the simplest portfolio maker.{" "}
         <Link className="underline" href="/how-to">
           How to use?
         </Link>
